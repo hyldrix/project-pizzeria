cec365409ca3250746d5aaf3940af0ffe90478ba
Szymon Skaskiewicz <szymon.skaskiewicz@gmail.com>
2024-02-03 21:49:48 +0200

    Added: New component responsible for Booking page (9.4)

---
+++ src/js/components/Booking.js
@@ -0,0 +1,34 @@
+import { select, templates } from '../settings.js';
+import utils from '../utils.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking {
+  constructor(element) {
+    const thisBooking = this;
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+
+  render(element) {
+    const thisBooking = this;
+    const generatedHTML = templates.bookingWidget();
+    const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper.appendChild(generatedDOM);
+    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+
+  }
+
+  initWidgets() {
+    const thisBooking = this;
+
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+
+
+  }
+}
+export default Booking;
